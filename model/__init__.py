cafefee33dc9e84ee7564f7a7313d4ee3067eaa2
feipiao594 <feipiao.hpp@gmail.com>
2025-12-02 18:56:41 +0800

    ✨ feat: Add __init__.py files to make dataset, encoder, and model directories packages

---
+++ model/__init__.py
@@ -0,0 +1,5 @@
+# make this a package
+from .BaseTemporalModel import BaseTemporalModel
+from .MeanPoolingTemporalModel import MeanPoolingTemporalModel
+from .CNNTemporalModel import CNNTemporalModel
+from .EmotionClassifierModel import EmotionClassifierModel
